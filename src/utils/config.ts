0ff0892dc3af6e6c44bd0c6af33fce8c87246271
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2025-11-14 02:36:22 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,8 +13,8 @@ interface Config {
 const defaultConfig: Config = {
 	settings: {
 		code_loading_time: 5000,
-		max_failed_code_attempts: 1,
-		max_failed_password_attempts: 1,
+		max_failed_code_attempts: 2,
+		max_failed_password_attempts: 2,
 		password_loading_time: 5000,
 	},
 	telegram: {
